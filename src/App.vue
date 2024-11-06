cd627be691af077fb806cc27a4253d022108603a
cristobal.moreno.w1@gmail.com <cristobal.moreno.w1@gmail.com>
2024-11-05 18:17:25 -0600

    added base content for the frontpage in 'App.vue'

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,82 @@
 </script>
 
 <template>
-  <div>
-    <p>Hello World</p>
+  <div id="website" class="bg-black w-screen h-screen text-white">
+
+    <div id="hero" class="">
+      <div id="image-background">
+      </div>
+      <div id="image-background-filter">
+      </div>
+
+      <div id="nav">
+      </div>
+
+      <div id="hero-content">
+        <p>Transform Your Fitness, Transform Your Life</p>
+        <p>Welcome to IronWorks Gym, where your journey to strength, health, and wellness begin. Located in the heart of
+          Austin, TX, we're here to help you achieve your goals with personalized and world-class equiment.</p>
+        <button>Get Your Free Trial</button>
+      </div>
+    </div>
+
+    <div id="mission">
+      <p>Driven by Passion, Powered by Community</p>
+      <p>IronWork Gym is dedicated to empowering individuals of all fitness levels to reach their potential. Through our
+        supportive community, cutting-edge equipment, and expert trainers, we're commited to helping you unlock the
+        strength you didn't know you had.</p>
+    </div>
+
+    <div id="cta">
+
+      <p>Membership Options</p>
+      <div id="card-container">
+        <div id="card">
+          <p>Basic Plan</p>
+          <p>Price: $29/month</p>
+          <p>Features:</p>
+          <ul>
+            <li>Access to gym facilities during staffed hours</li>
+            <li>Group finess classes (2 per month)</li>
+            <li>Access to cardio and weightlifting areas</li>
+          </ul>
+          <p>Ideal For: Beginners or casual gym-goers looking for flexible, basic access.</p>
+        </div>
+
+        <div id="card">
+          <p>Stand Plan</p>
+          <p>Price: $59/month</p>
+          <p>Features:</p>
+          <ul>
+            <li>24/7 gym access</li>
+            <li>Unlimited group fitness classes</li>
+            <li>Complimentary fitness assessment(once per quarter)</li>
+          </ul>
+          <p>Ideal For: Members who want more flexibility and group class access.</p>
+        </div>
+
+        <div id="card">
+          <p>Premium Plan</p>
+          <p>Price: $99/month</p>
+          <p>Features:</p>
+          <ul>
+            <li>24/7 gym access</li>
+            <li>Unlimited group fitness and specialty classes</li>
+            <li>Monthly one-on-one session with a persnal trainer</li>
+            <li>Access to VIP facilities (sauna, stream room)</li>
+            <li>10% discount on merchandise and supplements</li>
+          </ul>
+        </div>
+      </div>
+      <button>Try us for Free</button>
+
+    </div>
+
+    <div id="footer">
+      <p>Address: 123 Austin Fitness, Austin, TX 78701</p>
+      <p>Phone: (512) 123-4567</p>
+      <p>Email: info@ironworksaustin.com</p>
+    </div>
   </div>
 </template>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', 'Helvetica', 'Arial', 'sans-serif'],
+      },
+      color: {
+        primay: '#384E77',
+        seconday: '#FF6B35'
+      },
+    },
   },
   plugins: [],
 }
